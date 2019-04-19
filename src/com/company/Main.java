07c36293a424f5d8d0faf33f58e4f8af20295de9
agnesJ1 <agnesjett06@gmail.com>
2019-04-19 15:40:11 -0400

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,37 +3,33 @@ package com.company;
  *Agnes Jett
  *04/19/2019
  */
-
+import java.util.Scanner;
 public class Main {
-    public static void main(int n) {
-        int i, m = 0, flag = 0;
-        m = n / 2;
-        if (n == 0 || n == 1) {
-            System.out.println(n + " is not prime number");
-        } else {
-            for (i = 2; i <= m; i++) {
-                if (n % i == 0) {
-                    System.out.println(n + " is not prime number");
-                    flag = 1;
-                    break;
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            //String num = 0;
+            int i = 0;
+           int num = 0;
+
+            String primeNumbers = "";
+
+            for (i = 1; i <= 100; i++) {
+                int counter = 0;
+                for (num = i; num >= 1; num--) {
+                    if (i % num == 0) {
+                        counter = counter + 1;
+                    }
+                }
+                if (counter == 2) {
+                    //Appended the Prime number to the String
+                    primeNumbers = primeNumbers + i + " ";
+                    //primeNumbers= scanner.nextLine();
                 }
             }
-            if (flag == 0) {
-                System.out.println(n + " is prime number");
-            }
-        }//end of else
+            System.out.println("Prime numbers from 1 to 100 are :");
+            //num = scanner.nextLine();
+            System.out.println(primeNumbers);
+        }
     }
-
-    public static void main(String args[]) {
-        main(1);
-        main(2);
-        main(3);
-        main(4);
-        main(5);
-        main(6);
-        main(7);
-        main(8);
-        main(9);
-        main(10);
-    }
-}
